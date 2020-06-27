c43540745328e021812d56873538872428654f7f
Michal Kreft <michal.kreft@softwire.com>
2020-06-27 14:03:06 +0100

    Separate handling negative numbers

--- src/main/kotlin/com/gitTraining/Fibbonaci.kt
+++ src/main/kotlin/com/gitTraining/Fibbonaci.kt
@@ -10,8 +10,7 @@ fun computeFibbonaciNumber(position: Int?, recursion: Boolean = false): Int {
 
     if (notNullPosition == 0) return 0
     if (notNullPosition < 0) {
-        val positionIsOdd = notNullPosition % 2 == -1
-        return if (positionIsOdd) computeFibbonaciNumber(-notNullPosition) else (computeFibbonaciNumber(-notNullPosition) * -1)
+        return computeNegativeFibbonachi(notNullPosition)
     }
     
     var i = 1
@@ -45,4 +44,11 @@ fun recursiveFibbonachi(previous: Int, current: Int, stepsLeft: Int): Int {
         0 -> current
         else -> recursiveFibbonachi(current, previous + current, stepsLeft - 1)
     }
+}
+
+fun computeNegativeFibbonachi(position:Int): Int {
+    if (position >= 0) throw Exception("potition must be smaller than zero!")
+    val resultIsNegative = position % 2 == 0
+    val absoluteResult = computeFibbonaciNumber(-position)
+    return if (resultIsNegative) (absoluteResult * -1) else absoluteResult
 }
