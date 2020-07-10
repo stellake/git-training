c95bd4d8265a84db2e46e421bc640490dcca6ee4
Stella Veski <stella.veski@softwire.com>
2020-07-10 13:43:21 +0100

    Add a comment to test.

--- src/test/kotlin/com/gitTraining/RecursiveFibbonaciTest.kt
+++ src/test/kotlin/com/gitTraining/RecursiveFibbonaciTest.kt
@@ -8,6 +8,7 @@ class RecursiveFibbonaciTest {
     fun recursive_fibbonaci_works_for_small_numbers() {
         val inputs = listOf(1,2,3,4,5,6,7,8,9)
         val expectedOutputs = listOf(1,1,2,3,5,8,13,21,34)
+        // Comment
 
         for ((input, expectedOutput) in inputs.zip(expectedOutputs)) {
             assertThat(computeFibbonaciNumber(input, true)).isEqualTo(expectedOutput)
